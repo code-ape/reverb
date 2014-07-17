dac3b61a5a2f3b1e740e4d4e5248c2ae4490bd91
Ferris <ferris@ferrisellis.com>
2014-07-17 06:09:50 +0000

    Basic matcher class

--- token/java.go
+++ token/java.go
@@ -1,58 +1,13 @@
 package token
 
-func NewJavaEnvironment() *Environment {
-	return &Environment{}
-}
 
-type StartMultiCommentMatcher struct {
-	MatchingString       string
-	MatchingLen          int
-	AllowedEnvs          []string
-	MatchPriorChar       bool
-	AllowedPriorChar     []string
-	MatchFollowingChar   bool
-	AllowedFollowingChar []string
+func NewMultiCommentStartMatcher() *Matcher {
+	allowed_env = []string{"STRING", "CHAR"}
+	current_matcher_exp   = ""
+	prior_matcher_exp     = ""
+	following_matcher_exp = ""
 
-	CurrentString string
-	PriorChar     string
-	FollowingChar string
-}
-
-func NewStartMultiCommentMatcher() *StartMultiCommentMatcher {
-	return &StartMultiCommentMatcher{}
-}
-
-func (m *StartMultiCommentMatcher) Match(c string, env string) bool {
-	if !AllowedEnv(env, m) {
-		return false
-	}
-
-	AddChar(c, m)
-
-
-
-
-}
-
-func AllowedEnv(env string, m *ForwardSlashMatcher) bool {
-	for _,s := range m.AllowedEnvs {
-		if env == s {
-			return true
-		}
-	} else {
-		return false
-	}
-}
-
-func AddChar(c string, m *ForwardSlashMatcher) {
-	l := len(m.CurrentString)
-	if l == m.MatchingLen && m.FollowingChar != "" {
-		m.PriorChar := m.CurrentString[0]
-		m.CurrentString = m.CurrentString[1:l] + m.FollowingChar
-		m.FollowingChar = c
-	} else if l == m.MatchingLen && m.FollowingChar == "" {
-		m.FollowingChar = c
-	} else {
-		m.CurrentString = m.CurrentString + c
-	}
+	m := NewMatcher(allowed_env, current_matcher_exp, prior_matcher_exp,
+									following_matcher_exp)
+	return &Matcher{}
 }
--- token/matcher.go
+++ token/matcher.go
@@ -1,6 +1,91 @@
 package token
 
+import "regexp"
+
+
 type Matcher struct {
-	Sequence   string
-	AllowedEnv []string
+	AllowedEnvs        []string
+
+	CurrentMatchExp    string
+	PriorMatchExp      bool
+	FollowingMatchExp  bool
+
+	MainMatcher      *regexp.Regexp
+	PriorMatcher     *regexp.Regexp
+	FollowingMatcher *regexp.Regexp
+
+	CurrentString   string
+	PriorString     string
+	FollowingString string
+
+	CurrentMatch   bool
+	PriorMatch     bool
+	FollowingMatch bool
 }
+
+func NewMatcher(ae []string, cme, pme, fme string) *Matcher {
+	m := &Matcher{AllowedEnvs: ae, CurrentMatchExp: cme, PriorMatchExp pme,
+									FollowingMatchExp: fme}
+	m.CompileRegexp()
+	return m
+}
+
+func (m *Matcher) Reset(prior_string string) {
+	m.CurrentString   = ""
+	m.PriorString     = ""
+	m.FollowingString = ""
+	m.CurrentMatch    = false
+	m.PriorMatch      = false
+	m.FollowingMatch  = false
+	m.UpdatePriorMatch(prior_string)
+}
+
+
+func (m *Matcher) CompileRegexp() {
+	m.MainMatcher      = regexp.Compile(m.MatchExp)
+	m.PriorMatcher     = regexp.Compile(m.PriorMatchExp)
+	m.FollowingMatcher = regexp.Compile(m.FollowingMatcher)
+}
+
+func (m *Matcher) Match(c string, env string) bool {
+	if !m.AllowedEnv(env) || !m.PriorMatch {
+		return false
+	}
+
+	if m.CurrentMatch && m.FollowingString == "" {
+		temp_s  := m.CurrentString + c
+		attempt := m.MainMatcher(temp_s)
+		if attempt {
+			m.CurrentString = temp_s
+		} else {
+			m.UpdateFollowingMatch(c)
+		}
+	} else if m.CurrentMatch && m.FollowingMatcher != "" {
+		m.UpdateFollowingMatch(c)
+	}
+
+	return m.CheckMatches()
+	
+}
+
+
+func (m *Matcher) AllowedEnv(env string) bool {
+	for _,e := range m.AllowedEnvs {
+		if env == e { return true }
+	} else { return false }
+}
+
+func (m *Matcher) UpdateFollowingMatch(c string) {
+	m.FollowingString += c
+	m.FollowingMatch = m.FollowingMatcher(m.FollowingString)
+}
+
+func (m *Matcher) UpdatePriorMatch(c string) {
+	m.PriorString += c
+	m.PriorMatch = m.PriorMatcher(m.PriorString)
+}
+
+func (m *Matcher) CheckMatches(m *Matcher) {
+	return m.CurrentMatch && m.PriorMatch && m.FollowingMatch
+}
+
